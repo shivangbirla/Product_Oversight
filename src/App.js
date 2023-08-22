f75db41ec3ac875d661ab6833976e766eede4549
shivangbirla <birlashivang01@gmail.com>
2023-08-22 07:07:27 +0530

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
+  const [searchValue, setSearchValue] = useState(""); // Add state for search value
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar setSearchValue={setSearchValue} />
+      <Home searchValue={searchValue} />
+    </>
   );
 }
 
---
+++ src/data/data.js
@@ -0,0 +1,164 @@
+const products = [
+  {
+    productName: "Screw",
+    productId: "s1",
+    productDetails: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt ipsum sed dolor sollicitudin, non volutpat nunc pulvinar.",
+    zone: 1,
+    level: 1,
+    rowNo: 1,
+  },
+  {
+    productName: "Gear",
+    productId: "s2",
+    productDetails: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt ipsum sed dolor sollicitudin, non volutpat nunc pulvinar.",
+    zone: 1,
+    level: 1,
+    rowNo: 1,
+  },
+  {
+    productName: "Shaft",
+    productId: "s3",
+    productDetails: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt ipsum sed dolor sollicitudin, non volutpat nunc pulvinar.",
+    zone: 1,
+    level: 1,
+    rowNo: 1,
+  },
+  {
+    productName: "Clamp",
+    productId: "s4",
+    productDetails: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt ipsum sed dolor sollicitudin, non volutpat nunc pulvinar.",
+    zone: 1,
+    level: 1,
+    rowNo: 1,
+  },
+  {
+    productName: "Bolt",
+    productId: "s5",
+    productDetails: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt ipsum sed dolor sollicitudin, non volutpat nunc pulvinar.",
+    zone: 1,
+    level: 2,
+    rowNo: 2,
+  },
+  {
+    productName: "Nut",
+    productId: "s6",
+    productDetails: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt ipsum sed dolor sollicitudin, non volutpat nunc pulvinar.",
+    zone: 1,
+    level: 2,
+    rowNo: 2,
+  },
+  {
+    productName: "Rivet",
+    productId: "s7",
+    productDetails: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt ipsum sed dolor sollicitudin, non volutpat nunc pulvinar.",
+    zone: 1,
+    level: 2,
+    rowNo: 2,
+  },
+  {
+    productName: "Pinion",
+    productId: "s8",
+    productDetails: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt ipsum sed dolor sollicitudin, non volutpat nunc pulvinar.",
+    zone: 1,
+    level: 2,
+    rowNo: 2,
+  },
+  {
+    productName: "Axle",
+    productId: "s9",
+    productDetails: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt ipsum sed dolor sollicitudin, non volutpat nunc pulvinar.",
+    zone: 1,
+    level: 3,
+    rowNo: 3,
+  },
+  {
+    productName: "Valve",
+    productId: "s10",
+    productDetails: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt ipsum sed dolor sollicitudin, non volutpat nunc pulvinar.",
+    zone: 1,
+    level: 3,
+    rowNo: 3,
+  },
+  {
+    productName: "Lever",
+    productId: "s11",
+    productDetails: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt ipsum sed dolor sollicitudin, non volutpat nunc pulvinar.",
+    zone: 1,
+    level: 3,
+    rowNo: 3,
+  },
+  {
+    productName: "Cam",
+    productId: "s12",
+    productDetails: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt ipsum sed dolor sollicitudin, non volutpat nunc pulvinar.",
+    zone: 1,
+    level: 3,
+    rowNo: 3,
+  },
+  {
+    productName: "Flange",
+    productId: "s10",
+    productDetails: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt ipsum sed dolor sollicitudin, non volutpat nunc pulvinar.",
+    zone: 1,
+    level: 3,
+    rowNo: 4,
+  },
+  {
+    productName: "Flange",
+    productId: "s10",
+    productDetails: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt ipsum sed dolor sollicitudin, non volutpat nunc pulvinar.",
+    zone: 1,
+    level: 3,
+    rowNo: 4,
+  },
+  {
+    productName: "Crank",
+    productId: "s11",
+    productDetails: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt ipsum sed dolor sollicitudin, non volutpat nunc pulvinar.",
+    zone: 1,
+    level: 3,
+    rowNo: 4,
+  },
+  {
+    productName: "Gasket",
+    productId: "s12",
+    productDetails: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt ipsum sed dolor sollicitudin, non volutpat nunc pulvinar.",
+    zone: 1,
+    level: 3,
+    rowNo: 4,
+  },
+];
+
+export { products };
